89afc6d97b5bba645f79cd41e2482884101757e7
Nikita Kuzmin <nikita.kuzmaaa@gmail.com>
2024-04-21 09:12:51 +0300

    djhd

--- src/components/shared/Loader.tsx
+++ src/components/shared/Loader.tsx
@@ -1,13 +1,1 @@
-const Loader = () => (
-  <div className="flex-center w-full">
-    <img
-      src="/assets/icons/loader.svg"
-      alt="loader"
-      width={32}
-      height={32}
-      className="animate-spin"
-    />
-  </div>
-);
-
-export default Loader;
+const Loader=()=>(<div className="flex-center w-full"><img src="/assets/icons/loader.svg"alt="loader"width={32}height={32}className="animate-spin"/></div>);export default Loader;
--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -1,4 +1,4 @@
-export { default as Bottombar } from "./Bottombar";
-export { default as Loader } from "./Loader";
-export { default as PostCard } from "./PostCard";
-export { default as Topbar } from "./Topbar";
+export{default as Bottombar}from"./Bottombar";
+export{default as Loader}from"./Loader";
+export{default as PostCard}from"./PostCard";
+export{default as Topbar}from"./Topbar";
